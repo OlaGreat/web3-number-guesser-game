0bf26ae7b58a517a09ced633df5a503d78ef718b
OlaGreat <oladipupoolamilekan2@gmail.com>
2025-05-24 12:21:24 +0100

    [dev] added home screen content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
 export default function Home(){
-  ret
+  return (
+    <main>
+      <h6>Number Guessing Game</h6>
+    <p>Cau you guess the secret between 1 and 100</p>
+    <section>
+
+      <div>
+        <h6>Choose Difficulty</h6>
+        <p>select a difficulty</p>
+        <div>
+          <p>Choose Difficulty level to start playing game</p>
+          <div>
+            h
+          </div>
+        </div>
+      </div>
+    </section>
+    </main>
+  )
 }
