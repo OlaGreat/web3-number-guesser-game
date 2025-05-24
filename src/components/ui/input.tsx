d53bb2e5fe28817eab70b8759b2333f274d6318f
OlaGreat <oladipupoolamilekan2@gmail.com>
2025-05-24 14:24:46 +0100

    update

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,7 +3,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
     ({ className, type, ...props }, ref) => {
       return (
         <input
-          type={type}
+        type={type}
           className={"flex h-10 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 disabled:cursor-not-allowed disabled:opacity-50"}
           ref={ref}
           {...props}
